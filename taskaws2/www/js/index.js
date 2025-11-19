6c972567167823d3c5353783ff09d3f60c202c37
valeria <vsantanamegia01.cf@iesesteveterradas.cat>
2025-11-19 20:42:26 +0100

    version0.4 tasklist

--- taskaws2/www/js/index.js
+++ taskaws2/www/js/index.js
@@ -1,29 +1,104 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+    inicializarApp();
+}
+
+if (typeof cordova === 'undefined') {
+    $(document).ready(function() {
+        inicializarApp();
+    });
+}
+
+function inicializarApp() {
+    // Cargar tareas desde localStorage
+    function cargarTareas() {
+        const tareas = JSON.parse(localStorage.getItem('tareas')) || [];
+        $("#miLista").empty();
+        tareas.forEach(t => agregarTarea(t, false));
+    }
+
+    // Guardar tareas en localStorage
+    function guardarTareas() {
+        const tareas = [];
+        $("#miLista li").each(function() {
+            tareas.push($(this).find(".task-text").text());
+        });
+        localStorage.setItem('tareas', JSON.stringify(tareas));
+    }
+
+    // Dialog nueva tarea
+    $("#taskform").dialog({
+        autoOpen: false,
+        height: 200,
+        width: 350,
+        modal: true,
+        buttons: {
+            "OK": function() {
+                agregarTarea();
+                $(this).dialog("close");
+            },
+            "Cancelar": function() {
+                $(this).dialog("close");
+            }
+        }
+    });
+
+    // Dialog editar tarea
+    $("#editForm").dialog({
+        autoOpen: false,
+        height: 200,
+        width: 350,
+        modal: true,
+        buttons: {
+            "OK": function() {
+                const nuevaTarea = $("#editText").val().trim();
+                if (nuevaTarea !== "") {
+                    const li = $(this).data("li");
+                    li.find(".task-text").text(nuevaTarea);
+                    guardarTareas();
+                }
+                $(this).dialog("close");
+            },
+            "Cancelar": function() {
+                $(this).dialog("close");
+            }
+        }
+    });
+
+    // Event listener para el botón de añadir
+    document.getElementById('btnAdd').onclick = function() {
+        $("#tasktext").val("");
+        $("#taskform").dialog("open");
+    };
+
+    function agregarTarea(texto = null, guardar = true) {
+        let tarea = texto || $("#tasktext").val().trim();
+        if (tarea === "") { alert("Escribe una tarea"); return; }
+
+        const li = $("<li></li>");
+        const span = $("<span class='task-text'></span>").text(tarea);
+
+        const btnEditar = $("<button class='btnTask btn-edit'>Edit</button>");
+        btnEditar.click(function (event) {
+            const liActual = $(event.target).closest("li");
+            const textoActual = liActual.find(".task-text").text();
+            $("#editText").val(textoActual);
+            $("#editForm").data("li", liActual).dialog("open");
+        });
+
+        const btnEliminar = $("<button class='btnTask btn-delete'>X</button>");
+        btnEliminar.click(function (event) {
+            $(event.target).closest("li").remove();
+            guardarTareas();
+        });
+
+        li.append(span).append(btnEditar).append(btnEliminar);
+        $("#miLista").append(li);
+
+        if (guardar) guardarTareas();
+    }
+
+    cargarTareas();
 }
